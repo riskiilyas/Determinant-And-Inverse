653afc7bcdd508bb98b03cd27d4186f0862a5199
riskiilyas <riskiilyas03@gmail.com>
2022-09-16 20:33:07 +0700

    Add User Input

--- Main.py
+++ Main.py
@@ -1,10 +1,18 @@
 import Determinant
 import Invers
 
-for row in Invers.inverse([[1, 1, 0, 2], [0, 0, 3, 7], [1, 2, 4, 1], [0, 3, 0, 1]]):
+size = int(input('How much is the size? '))
+matrix = []
+
+for i in range(0, size):
+    row = []
+    for j in range(0, size):
+        row.append(int(input('(' + str(i) + ', ' + str(j) + ') ')))
+
+    matrix.append(row)
+
+print('Determinant: ' + str(Determinant.determinant(matrix)))
+print('Invers: ')
+for row in Invers.inverse(matrix):
     print(row)
 
-for row in Invers.inverse([[2, 1], [1, 1]]):
-    print(row)
-
-print(Determinant.determinant([[1, 1, 0, 2], [0, 0, 3, 7], [1, 2, 4, 1], [0, 3, 0, 1]]))
