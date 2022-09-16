eb161a83901de1f6a88905762995e15b7e4c04cd
riskiilyas <riskiilyas03@gmail.com>
2022-09-17 05:19:25 +0700

    Update Documentation

--- Determinant.py
+++ Determinant.py
@@ -5,43 +5,43 @@ def determinant(matrix):
         return (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1])
     # Return the element if matrix 1x1
     elif len(matrix) == 1:
-        return matrix[0]
+        return matrix[0][0]
 
     # Calculation if matrix is greater than 2x2
 
-    # Make result variable to calculate total of the sub-matrix determinant
+    # Make result variable to calculate total of the minor matrices determinant
     result = 0
 
     # Cofactor Expansion using First Row
     for index, num in enumerate(matrix[0]):
         # Matrix to store the Minor of a(index) entry
-        newMatrix = []
+        minorMatrix = []
 
-        # The entry value
+        # The entry value (negative if the position is even)
         entry = num if index % 2 == 0 else -num
 
         # Iterate the Row
         for indexRow, row in enumerate(matrix):
 
-            # Skip if the column is the first
+            # Skip if the column is the first (that's the cofactor's entry row)
             if indexRow == 0:
                 continue
 
             # Matrix to store the Minor Row
-            rowMatrix = []
+            rowMinor = []
 
             # Iterate the columns of the indexRow-th row
             for indexColumn, value in enumerate(row):
 
                 # Add the element if the Column index is not the same with the entry's
                 if indexColumn != index:
-                    rowMatrix.append(value)
+                    rowMinor.append(value)
 
             # Add the row to the matrix
-            newMatrix.append(rowMatrix)
+            minorMatrix.append(rowMinor)
 
         # Add the result and call the determinant function recursively until the size is 2x2
-        result += (entry * determinant(newMatrix))
+        result += (entry * determinant(minorMatrix))
 
     # The result is the sum of all determinants of the minors
     return result
